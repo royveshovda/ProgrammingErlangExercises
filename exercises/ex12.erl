1d5ea0959e28f402c1245dddea0a7c94b89f9c88
Roy Veshovda <roy@veshovda.no>
2014-01-26 08:56:07 +0200

    Exercise 12

--- exercises/ex12.erl
+++ exercises/ex12.erl
@@ -1,6 +1,6 @@
 -module(ex12).
 
--export([start/2, max/1]).
+-export([start/2, max/1, max/0, wait/0, wait_for_message/1, max_ring/2]).
 
 start(AnAtom, Fun) ->
 	undefined = whereis(AnAtom),
@@ -8,6 +8,8 @@ start(AnAtom, Fun) ->
 	register(AnAtom, Pid),
     Pid.
 
+max() ->
+    max(100000).
 
 %% max(N) 
 
@@ -34,3 +36,55 @@ wait() ->
 
 for(N, N, F) -> [F()];
 for(I, N, F) -> [F()|for(I+1, N, F)].
+
+%% Write a ring benchmark. Create N processes in a ring. Send a message round in a ring M times so that a total of N * M messages get sent.
+%% Time how long this takes for different values of N and M.
+
+%% TODO: Make measurements
+
+max_ring(Processes, Messages) ->
+    Pid0 = self(),
+    PidStart = for_spawn(Pid0, Processes, Processes),
+    io:format("All spawned~n"),
+
+    pass_messages(PidStart, Messages),
+    
+    PidStart ! die,
+    receive
+        die -> io:format("Die received~n")
+    end,
+    io:format("Done!~n").
+
+for_spawn(PidToPassOnTo, 1, _) ->
+    PidToReturn = spawn(fun() -> wait_for_message(PidToPassOnTo) end),
+    PidToReturn;
+for_spawn(PidToPassOnTo, Number, Max) ->
+    NewPid = spawn(fun() -> wait_for_message(PidToPassOnTo) end),
+    for_spawn(NewPid, Number-1, Max).
+
+pass_messages(PidStart, 0) ->
+    pass_message(PidStart);
+pass_messages(PidStart, Messages) ->
+    pass_message(PidStart),
+    io:format("Ping~n"),
+    pass_messages(PidStart, Messages-1).
+
+pass_message(PidStart) ->
+    PidStart ! pass,
+    receive
+        pass -> io:format("Token received~n")
+    end.
+    
+
+%% Function to spawn and connect N processes
+%% Function to time and send M messages
+
+wait_for_message(PidToPassOnTo) ->
+    receive
+        die ->
+            PidToPassOnTo ! die;
+        pass ->
+            PidToPassOnTo ! pass, wait_for_message(PidToPassOnTo)
+        end.
+
+
