b79c88ef383e36e88e5b0186405d63baea7ced84
Roy Veshovda <roy@veshovda.no>
2014-01-27 07:51:55 +0200

    Exercise 13

--- exercises/ex13.erl
+++ exercises/ex13.erl
@@ -1,30 +1,55 @@
 -module(ex13).
--export([my_spawn/3, sleeper/1]).
+-export([my_spawn1/3, my_spawn2/3, my_spawn3/4, sleeper/1]).
 
-my_spawn(Mod, Func, Args) ->
+my_spawn1(Mod, Func, Args) ->
     statistics(wall_clock), %% calles to create a startTime
 
-	{Pid, Ref} = spawn_monitor(Mod, Func, Args),
+	Pid = spawn(Mod, Func, Args),
+	Ref = monitor(process, Pid),
 	receive
     	{'DOWN', Ref, process, Pid, Why} ->
-        	io:format(" ~p died with:~p~n",[Pid, Why])
-	end,
-    {_, Time} = statistics(wall_clock),
-    io:format("Ran for =~p milliseconds~n",[Time]).
+        	io:format("~p died with: ~p~n",[Pid, Why]),
+    		{_, RunTime} = statistics(wall_clock),
+    		io:format("Ran for = ~p milliseconds~n",[RunTime])
+	end.
 
+my_spawn2(Mod, Func, Args) ->
+    Pid = spawn(Mod, Func, Args),
+    on_exit(Pid, fun(Why) -> io:format(" ~p died with:~p~n",[Pid, Why]) end).
+
+my_spawn3(Mod, Func, Args, Time) ->
+	statistics(wall_clock), %% calles to create a startTime
+
+	{Pid, Ref} = spawn_monitor(Mod, Func, Args),
+
+	receive
+    	{'DOWN', Ref, process, Pid, Why} ->
+        	io:format(" ~p died with: ~p~n",[Pid, Why]),
+        	{_, RunTime} = statistics(wall_clock),
+		    io:format("Ran for = ~p milliseconds~n",[RunTime])
+		after Time ->
+			exit(Pid, terminated),
+			io:format("Terminated")
+	end.
 
 sleeper(SleepTimeInMilliseconds) ->
 	timer:sleep(SleepTimeInMilliseconds),
 	exit("Felt like it").
 
+on_exit(Pid, Fun) ->
+	spawn(fun() ->
+			statistics(wall_clock),
+			Ref = monitor(process, Pid),
+				receive
+            		{'DOWN', Ref, process, Pid, Why} ->
+                    	Fun(Why),
+                    	{_, RunTime} = statistics(wall_clock),
+                    	io:format("Ran for =~p milliseconds~n",[RunTime])
+				end
+			end).
+
+
 %% Exercises
-%% DONE: 1. Write a function my_spawn(Mod, Func, Args) that behaves like spawn(Mod, Func, Args) but with one difference.
-%% If the spawned process dies, a message should be printed saying why the process died and how long the process lived for before it died.
-
-%% 2. Solve the previous exercise using the on_exit function shown earlier in this chapter.
-
-%% 3. Write a function my_spawn(Mod, Func, Args, Time) that behaves like spawn(Mod, Func, Args) but with one difference.
-%% If the spawned process lives for more than Time seconds, it should be killed.
 
 %% 4. Write a function that creates a registered process that writes out "I'm still running" every five seconds.
 %% Write a function that monitors this process and restarts it if it dies.
@@ -35,3 +60,5 @@ sleeper(SleepTimeInMilliseconds) ->
 
 %% 6. Write a function that starts and monitors several worker processes.
 %% If any of the worker processes dies abnormally, kill all the worker processes and restart them all.
+
+
