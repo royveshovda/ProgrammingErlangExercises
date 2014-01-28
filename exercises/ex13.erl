6d7bc92c9802fb66c3f06fc8ec3aa73704798530
Roy Veshovda <roy@veshovda.no>
2014-01-28 14:03:51 +0200

    Exercise 13

--- exercises/ex13.erl
+++ exercises/ex13.erl
@@ -1,5 +1,5 @@
 -module(ex13).
--export([my_spawn1/3, my_spawn2/3, my_spawn3/4, sleeper/1, i_am_alive/0, monitor/0, start/0]).
+-export([my_spawn1/3, my_spawn2/3, my_spawn3/4, sleeper/1, i_am_alive/0, i_am_alive1/0, i_am_alive2/0, i_am_alive3/0, monitor/0, start/0, restart_worker1/0, keep_alive/4, on_exit/2]).
 
 my_spawn1(Mod, Func, Args) ->
     statistics(wall_clock), %% calles to create a startTime
@@ -55,6 +55,27 @@ i_am_alive() ->
 			i_am_alive()
 	end.
 
+i_am_alive1() ->
+	receive
+		after 4000 ->
+			io:format("( ~p ) I am alive (1).~n", [self()]),
+			i_am_alive1()
+	end.
+
+i_am_alive2() ->
+	receive
+		after 5000 ->
+			io:format("( ~p ) I am alive (2).~n", [self()]),
+			i_am_alive2()
+	end.
+
+i_am_alive3() ->
+	receive
+		after 6000 ->
+			io:format("( ~p ) I am alive (3).~n", [self()]),
+			i_am_alive3()
+	end.
+
 monitor() ->
 	Ref = erlang:monitor(process, alive),
 	receive
@@ -68,17 +89,18 @@ start() ->
 	Pid = spawn(ex13, i_am_alive, []),
 	register(alive, Pid).
 
+keep_alive(Name, Mod, Fun, Args) ->
+	register(Name, Pid = spawn(Mod, Fun, Args)),
+	on_exit(Pid, fun(_Why) -> keep_alive(Name, Mod, Fun, Args) end).
+
+restart_worker1() ->
+	keep_alive(alive1, ex13, i_am_alive1, []),
+	keep_alive(alive2, ex13, i_am_alive2, []),
+	keep_alive(alive3, ex13, i_am_alive3, []).
+	
 
 
 %% Exercises
-
-%% 4. Write a function that creates a registered process that writes out "I'm still running" every five seconds.
-%% Write a function that monitors this process and restarts it if it dies.
-%% Start the global process and the monitor process. Kill the global process and check that it has been restarted by the monitor process.
-
-%% 5. Write a function that starts and monitors several worker processes.
-%% If any of the worker processes dies abnormally, restart it.
-
 %% 6. Write a function that starts and monitors several worker processes.
 %% If any of the worker processes dies abnormally, kill all the worker processes and restart them all.
 
